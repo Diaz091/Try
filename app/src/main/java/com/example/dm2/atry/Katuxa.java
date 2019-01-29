753446cc747801eafca7d5429b254934ed9b6059
Diaz091 <unai_01012@hotmail.com>
2019-01-30 00:26:04 +0100

    0

--- app/src/main/java/com/example/dm2/atry/Katuxa.java
+++ app/src/main/java/com/example/dm2/atry/Katuxa.java
@@ -22,40 +22,19 @@ public class Katuxa extends AppCompatActivity {
         setContentView( R.layout.activity_katuxa );
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-
         arantza18 = MediaPlayer.create(this, R.raw.arantza1); // "Reproductor del Sonido"
-        //final ImageButton playAndPause = (ImageButton)  this.findViewById(R.id.bottom); // Botón para Pause && Play
-        final TextView    time2 = (TextView ) this.findViewById( R.id.time );
-
-
-       arantza18.start(); // Iniciar el Sonido al iniciar la actividad
-        //playAndPause.setBackgroundResource( R.mipmap.ic_play); // Icono Botón al inicio de la actividad
+        arantza18.start(); // Iniciar el Sonido al iniciar la actividad
 
         arantza18.setOnCompletionListener( new MediaPlayer.OnCompletionListener() {
+
             @Override
             public void onCompletion( MediaPlayer mp ) {
                 finish();
-                Intent lorategi = new Intent( Katuxa.this , Lorategia.class );
-                startActivity( lorategi );
+                Intent lorategia = new Intent( Katuxa.this , Lorategia.class );
+                startActivity( lorategia );
                 finish();
             }
         } );
-
-
-        /*
-         playAndPause.setOnClickListener( new View.OnClickListener() { // Listener para cambiar el Icono
-            @Override                                                 // Dependiendo de si esta en Pause || Pause
-            public void onClick(View v) {
-                if ( arantza18.isPlaying() ){ // Comprueba si el Reproductor esta reproduciendo algo
-                    arantza18.pause();
-                    time2.setText( " Time "  + arantza18.getCurrentPosition());
-                    playAndPause.setBackgroundResource( R.mipmap.ic_pause);
-                }else{
-                    playAndPause.setBackgroundResource( R.mipmap.ic_play);
-                    arantza18.start();
-                }
-            }
-        });*/
 
 
     }
