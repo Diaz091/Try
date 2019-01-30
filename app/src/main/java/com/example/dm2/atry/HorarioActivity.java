d30782be2dea3a726384d8bee0bd2421c9077892
Unai <unai_01012@hotmail.com>
2019-01-30 10:02:39 +0100

    Horario

---
+++ app/src/main/java/com/example/dm2/atry/HorarioActivity.java
@@ -0,0 +1,67 @@
+package com.example.dm2.atry;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.dm2.atry.horarios.HorariosListAdapter;
+import com.marck.renfeApi.contentSelector.Selector;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+
+public class HorarioActivity extends AppCompatActivity {
+    public static final int LLODIO_ST = 0x00f2;
+    public static final String DIRECCION = "DIRECCION";
+    public static final int ST_LLODIO = 0x00f1;
+    private String[][] data;
+    private Selector s;
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_horario );
+
+        Bundle b = getIntent().getExtras();
+        int chose = b.getInt( DIRECCION );
+        String file = MainActivity.ST_LLODIO_H_FILE;
+        if( chose == LLODIO_ST )
+            file = MainActivity.LLODIO_ST_H_FILE;
+        try {
+            s = Selector.build().setDocument( new BufferedReader( new FileReader( getFileStreamPath( file ) ) ) );
+
+            String info = String.format( getString( R.string.h_last_update ), s.getDate() );
+            data = null;
+            ( ( TextView ) findViewById( R.id.h_info ) ).setText( s.getOrigen() + " - " + s.getDestino() );
+            ( ( TextView ) findViewById( R.id.h_date ) ).setText( info );
+            Toast.makeText( getApplicationContext(), "Leyendo los horarios", Toast.LENGTH_LONG ).show();
+            new Handler().postDelayed(  new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        data = s.getTabla();
+                        HorariosListAdapter ad = new HorariosListAdapter( HorarioActivity.this, R.id.h_lista, data );
+                        ( ( ListView )findViewById( R.id.h_lista ) ).setAdapter( ad );
+                    } catch ( Selector.EmptyDocumentException e ) {
+                        Log.e( "Horarios", "Error de lectura", e );
+                    }
+                }
+            } , 300 );
+
+
+
+        } catch ( FileNotFoundException e ) {
+           Toast.makeText( getApplicationContext(), "No se han encontrado los horario", Toast.LENGTH_SHORT ).show();
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/example/dm2/atry/MainActivity.java
+++ app/src/main/java/com/example/dm2/atry/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.dm2.atry;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
+import android.widget.Toast;
+
+import com.marck.renfeApi.RenfeRequest;
+import com.marck.renfeApi.params.RequestParams;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String LLODIO_ST_H_FILE = "llodio_st.h";
+    public static final String ST_LLODIO_H_FILE = "st_llodio.h";
+    public static final String MAP_OFFLINE_FILE = "mbgl-offline.db";
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
@@ -21,5 +36,66 @@ public class MainActivity extends AppCompatActivity {
     public void newAct( View view ) {
         Intent  newAct = new Intent( this , Katuxa.class );
         startActivity( newAct );
+    }
+
+    //    descarga de horarios
+    private void descargaDeHorarios() throws RenfeRequest.WrongDateFormatException {
+        Date toDay = new Date();
+        SimpleDateFormat format = new SimpleDateFormat( "dd-MM-yyyy" );
+//        creamos las variables
+        final RequestParams st_llodio = new RequestParams()
+                .put( RequestParams.HORA_DESTINO, "26" )
+                .put( RequestParams.DESTINO, "13106" ) // santa cruz
+                .put( RequestParams.ORIGEN, "13104" ) // llodio
+                .put( RequestParams.HORA_ORIGEN, "2" )
+                .put( RequestParams.FECHA, RenfeRequest.formatDate( format.format( toDay ) ) );
+
+        new Handler().postDelayed( new Runnable() {
+            @Override
+            public void run() {
+                RenfeRequest r = RenfeRequest.build();
+                try {
+                    r.setParams( st_llodio );
+                    r.buildURL();
+                    r.connect( MainActivity.this.openFileOutput( ST_LLODIO_H_FILE, MODE_PRIVATE ) );
+                } catch ( RequestParams.ParamNotFoundException e ) {
+                    Log.e( "HORARIOS", "Parametros erroneos", e );
+                } catch ( FileNotFoundException e ) {
+                    Log.e( "HORARIOS", "File not found", e );
+                } catch ( MalformedURLException e ) {
+                    Log.e( "HORARIOS", "URL erronea", e );
+                } catch ( IOException e ) {
+                    Log.e( "HORARIOS", "", e );
+                }
+            }
+        },500 );
+
+        final RequestParams llodio_st = new RequestParams()
+                .put( RequestParams.HORA_DESTINO, "26" )
+                .put( RequestParams.DESTINO, "13104" ) // santa cruz
+                .put( RequestParams.ORIGEN, "13106" ) // llodio
+                .put( RequestParams.HORA_ORIGEN, "2" )
+                .put( RequestParams.FECHA, RenfeRequest.formatDate( format.format( toDay ) ) );
+        new Handler().postDelayed( new Runnable() {
+            @Override
+            public void run() {
+                RenfeRequest r = RenfeRequest.build();
+                try {
+                    r.setParams( llodio_st );
+                    r.buildURL();
+                    r.connect( MainActivity.this.openFileOutput( LLODIO_ST_H_FILE, MODE_PRIVATE ) );
+                    Toast.makeText( MainActivity.this.getApplicationContext(),
+                            "Horarios Actualizados", Toast.LENGTH_SHORT ).show();
+                } catch ( RequestParams.ParamNotFoundException e ) {
+                    Log.e( "HORARIOS", "Parametros erroneos", e );
+                } catch ( FileNotFoundException e ) {
+                    Log.e( "HORARIOS", "File not found", e );
+                } catch ( MalformedURLException e ) {
+                    Log.e( "HORARIOS", "URL erronea", e );
+                } catch ( IOException e ) {
+                    Log.e( "HORARIOS", "", e );
+                }
+            }
+        }, 1000 );
     }
 }
