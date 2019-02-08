514e88b7f8d0890949830c17bc3ab71b59d09c8c
Unai <unai_01012@hotmail.com>
2019-02-08 11:52:56 +0100

    New Things

--- app/src/main/java/com/example/dm2/atry/FinFuentes.java
+++ app/src/main/java/com/example/dm2/atry/FinFuentes.java
@@ -7,12 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class FinFuentes extends AppCompatActivity {
 
     private MediaPlayer arantza21;
     private boolean repitiendo = false;
     private ImageView jarraitu , repetir;
+    private TextView txtJar , txtRep;
+
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
@@ -20,10 +23,22 @@ public class FinFuentes extends AppCompatActivity {
         setContentView( R.layout.activity_fin_fuentes );
         getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         arantza21 = MediaPlayer.create( this , R.raw.arantza21 );
+        txtJar = findViewById( R.id.txtJarraitu );
+        txtRep = findViewById( R.id.txtErrepi );
 
         if(!repitiendo) {
             arantza21.start();
         }
+
+        arantza21.setOnCompletionListener( new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion( MediaPlayer mp ) {
+                jarraitu.setVisibility( View.VISIBLE );
+                repetir.setVisibility( View.VISIBLE );
+                txtJar.setVisibility( View.VISIBLE );
+                txtRep.setVisibility( View.VISIBLE );
+            }
+        });
 
         jarraitu = findViewById( R.id.jarraitu );
         jarraitu.setOnClickListener( new View.OnClickListener() {
--- app/src/main/java/com/example/dm2/atry/Urmaela.java
+++ app/src/main/java/com/example/dm2/atry/Urmaela.java
@@ -1,13 +1,35 @@
 package com.example.dm2.atry;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class Urmaela extends AppCompatActivity {
+
+
+    private Button sakatu;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_urmaela );
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        sakatu = findViewById( R.id.botSakatu );
+
+        sakatu.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                Intent skate = new Intent( Urmaela.this , Skate.class );
+                startActivity( skate );
+
+            }
+        } );
+
+
     }
 }
